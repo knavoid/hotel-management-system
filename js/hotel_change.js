23f110ee1b742dd89a0cb1e6d5ba6d9f3322ac01
nOctaveLay <wjddkdml1124@naver.com>
2020-11-27 13:22:05 +0900

    Update hotel_change.js
    room의 개수 초과시 버튼 누를 수 없게 만듬

--- js/hotel_change.js
+++ js/hotel_change.js
@@ -1,6 +1,15 @@
 /* 누를 때마다 버튼의 색을 바꿔주는 method. 
 눌려있으면 count를 1로 바꾸고, 그렇지 않으면 0으로 둔다.*/
 
+function showRoom(){
+    var con = document.getElementsByClassName('hotel_room');
+    if (con.style.display == 'none'){
+        con.style.display = 'block';
+    }
+    console.log(con.style.display);
+}
+
+
 function setBtnColor(e) {
     var target = e.target,
         count = +target.dataset.count;
@@ -10,17 +19,14 @@ function setBtnColor(e) {
      target.dataset.count = count === 1 ? 0 : 1;
   }
 
-/*count가 1인것만 form을 이용해서 넘겨주도록 만들려고 한다. */
-
+/*만약 room의 갯수가 초과하면, 더 이상 버튼을 누를 수 없게끔 만든다. */
 function select_room(){
-    var selection = $('input[name="rooms[]"]');
-    var new_selection = new Array();
-    for (var i in selection) {
-        var count = array[i].dataset.count;
-        console.log(count);
-        if (count === 1) {
-            new_selection.push(array[i]);
-        }
+    var NOR = parseInt(document.getElementById("NOR").innerHTML); 
+    // var NOR = 3;
+    var selection = $('input[name="rooms[]"]:checkbox:checked');
+    if ($("input[id=room]:checkbox:checked").length >= NOR) {
+        var input_room = $('input[name="rooms[]"]');
+        input_room.attr('disabled',true);
     }
 }
 
